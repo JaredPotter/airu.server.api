b0a6cbafc464e446a3e71d44b5d36a3b4392cf6b
TJHooker33 <rolyat.wilson@gmail.com>
2015-11-29 18:31:27 -0700

    removing packages, don't worry they will come back when you build

--- server_api/packages/AttributeRouting.WebApi.Hosted.3.5.6/tools/install.ps1
+++
@@ -1,8 +0,0 @@
-﻿param($installPath, $toolsPath, $package, $project)
-
-# Remove the App_Start file for other languages
-if ($project.Type -eq "C#") {
-	$project.ProjectItems.Item("AttributeRoutingConfig.vb").Delete();
-} elseif ($project.Type -eq "VB.NET")  {
-	$project.ProjectItems.Item("AttributeRoutingConfig.cs").Delete();
-}
