99a05065e7c2de3283eebeb3cb0728a172ac6357
Jared Potter <u0729256@utah.edu>
2015-11-30 09:26:11 -0700

    Adding a build file to build and publish the server_api project. Once done, it should be available on the live internet.

---
+++ publish_build.cmd
@@ -0,0 +1,2 @@
+call "%VS120COMNTOOLS%\vsvars32.bat"
+call msbuild server_api/server_api.sln /p:DeployOnBuild=true /p:PublishProfile=deployment_package /p:WebPublishMethod=FileSystem /p:publishUrl=C:\airu.server.api\server_api_deployment_package
